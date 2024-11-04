ed31e9b05bc626122579d47de9a32a3e1d0decea
leandrop22 <leandropetricca123@gmail.com>
2024-11-04 00:57:08 -0300

    second commit

--- src/main/java/com/example/apiMagneto/ApiMagnetoApplication.java
+++ src/main/java/com/example/apiMagneto/ApiMagnetoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiMagnetoApplication {
+public class eApiMagnetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiMagnetoApplication.class, args);
